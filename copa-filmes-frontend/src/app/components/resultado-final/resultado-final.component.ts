b64e885937bdf2e0eb538220eead355a82fe5d83
Gabriel Barros <gabriel.fbarros@yahoo.com.br>
2019-02-03 05:13:20 -0200

    Conclui tela de resultados.

--- copa-filmes-frontend/src/app/components/resultado-final/resultado-final.component.ts
+++ copa-filmes-frontend/src/app/components/resultado-final/resultado-final.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-resultado-final',
@@ -11,7 +12,7 @@ export class ResultadoFinalComponent implements OnInit {
   public campeao: string;
   public vice: string;
 
-  public constructor(private route: ActivatedRoute) {
+  public constructor(private route: ActivatedRoute, private router: Router) {
     this.route.queryParams.subscribe(params => {
       this.campeao = params["campeao"];
       this.vice = params["vice"];
@@ -19,6 +20,10 @@ export class ResultadoFinalComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  gerarNovoCampeonato(): void{
+    this.router.navigate(["selecao-filmes"]);
   }
 
 }
